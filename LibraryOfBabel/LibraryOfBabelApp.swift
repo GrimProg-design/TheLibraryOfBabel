748a45bdfc9c0f95b241c8f5fa92b805b74434b1
GrimProg <vavilovila01@gmail.com>
2026-02-21 11:25:40 +0600

    Initial Commit

---
+++ LibraryOfBabel/LibraryOfBabelApp.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryOfBabelApp.swift
+//  LibraryOfBabel
+//
+//  Created by Вавилов Илья on 21/2/26.
+//
+
+import SwiftUI
+
+@main
+struct LibraryOfBabelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LibraryOfBabelTests/LibraryOfBabelTests.swift
@@ -0,0 +1,17 @@
+//
+//  LibraryOfBabelTests.swift
+//  LibraryOfBabelTests
+//
+//  Created by Вавилов Илья on 21/2/26.
+//
+
+import Testing
+@testable import LibraryOfBabel
+
+struct LibraryOfBabelTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
