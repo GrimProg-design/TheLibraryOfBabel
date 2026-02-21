f4c16c788e256b0efee63c35cda9b96923950467
GrimProg <vavilovila01@gmail.com>
2026-02-21 11:36:01 +0600

    Created Library of Babel

--- LibraryOfBabel/ContentView.swift
+++ LibraryOfBabel/ContentView.swift
@@ -9,13 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            Tab("Library", systemImage: "building.columns") {
+                
+            }
+            
+            Tab("Map", systemImage: "map") {
+                
+            }
         }
-        .padding()
     }
 }
 
